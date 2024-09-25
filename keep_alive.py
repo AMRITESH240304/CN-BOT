5c5501a192a6323e9922b1ac1268fea3bc7a2e26
rjk51 <5477053-rjk51@users.noreply.replit.com>
2024-09-25 19:17:48 +0000

    flask

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,14 @@
-from fastapi import FastAPI
-import uvicorn
+from flask import Flask
 from threading import Thread
 
-app = FastAPI()
+app = Flask('')
 
-@app.get("/", methods=["GET", "HEAD"])
+@app.route('/')
 def home():
-    return {"message": "Coding Ninjas SRM"}
+    return "<b>Coding Ninjas SRM</b>"
 
 def run():
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+  app.run(host='0.0.0.0',port=8080)
 
 def keep_alive():
     t = Thread(target=run)
